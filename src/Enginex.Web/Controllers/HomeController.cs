93acee39b18b043d191703b8a7a635b6a793e232
michalkoh <michalkoh@gmail.com>
2019-11-03 22:46:36 +0100

    Add project files.

---
+++ src/Enginex.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Enginex.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
