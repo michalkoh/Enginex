77dd6087e9fc4eae7f3d61dc2b232f6ee0ed4f35
michalkoh <michalkoh@gmail.com>
2019-12-25 23:48:09 +0100

    Sections Contact and About were added. Structure of the solution fixed.

---
+++ src/Web/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Enginex.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
