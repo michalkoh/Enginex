e8c7ed438f1c9f762305f49ccb3df3a751f6b271
michalkoh <michalkoh@gmail.com>
2020-01-09 23:29:46 +0100

    Added Prduct List View.

---
+++ src/Web/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Enginex.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
