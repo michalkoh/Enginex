7e4537d5decc56cae703a7e493eb91b4353948f0
michalkoh <michalkoh@gmail.com>
2020-04-18 15:00:34 +0200

    Login functionality via Google added.

--- src/Web/Controllers/AccountController.cs
+++ src/Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Enginex.Web.ViewModels.Admin;
 using Microsoft.AspNetCore.Authorization;
@@ -10,33 +11,82 @@ namespace Enginex.Web.Controllers
     public class AccountController : Controller
     {
         private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AccountController(SignInManager<IdentityUser> signInManager)
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
         {
             this.signInManager = signInManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
         public async Task<IActionResult> Login(string returnUrl)
         {
             var externalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            var model = new LoginViewModel(returnUrl, externalLogins);
-            return View(model);
+            var loginViewModel = new LoginViewModel(returnUrl, externalLogins);
+            return View(loginViewModel);
         }
 
         [HttpPost]
-        public IActionResult ExternalLogin(string provider, string returnUrl)
+        public IActionResult Login(string provider, string returnUrl)
         {
             var redirectUrl = Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl });
-            var properties = this.signInManager
-                .ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            var properties = this.signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
             return new ChallengeResult(provider, properties);
         }
 
         public async Task<IActionResult> ExternalLoginCallback(string? returnUrl = null, string? remoteError = null)
         {
-            await Task.CompletedTask;
-            return LocalRedirect(returnUrl);
+            returnUrl ??= Url.Content("~/");
+
+            var externalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            var loginViewModel = new LoginViewModel(returnUrl, externalLogins);
+
+            if (remoteError == null)
+            {
+                ModelState.AddModelError(string.Empty, $"Nastala chyba pri prihlasovaní: {remoteError}");
+                return View("Login", loginViewModel);
+            }
+
+            var info = await this.signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                ModelState.AddModelError(string.Empty, "Nepodarilo sa získať prihlasovacie údaje od externého poskytovateľa.");
+                return View("Login", loginViewModel);
+            }
+
+            var signInResult = await this.signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+
+            if (signInResult.Succeeded)
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+            ////if (email != null)
+            ////{
+            ////    var user = await this.userManager.FindByEmailAsync(email);
+            ////    if (user == null)
+            ////    {
+            ////        user = new IdentityUser
+            ////        {
+            ////            UserName = info.Principal.FindFirstValue(ClaimTypes.Email),
+            ////            Email = info.Principal.FindFirstValue(ClaimTypes.Email)
+            ////        };
+
+            ////        await this.userManager.CreateAsync(user);
+            ////    }
+
+            ////    await this.userManager.AddLoginAsync(user, info);
+            ////    await this.signInManager.SignInAsync(user, isPersistent: false);
+
+            ////    return LocalRedirect(returnUrl);
+            ////}
+
+            ViewBag.ErrorTitle = $"Email claim not received from: {info.LoginProvider}";
+            ViewBag.ErrorMessage = "Please contact support on Pragim@PragimTech.com";
+
+            return View("Error");
         }
     }
 }
