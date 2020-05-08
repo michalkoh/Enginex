e5248a83ff86462b869a4bcf8437ce00ec2a0b1e
michalkoh <michalkoh@gmail.com>
2020-05-08 14:57:13 +0200

    SQL Script for seeding products updated.

--- sql/SeedData.sql
+++
@@ -1,8 +0,0 @@
-
-insert into Categories (Name_sk, Name_en) 
-values 
-	('Kovoobrábacie stroje', 'Metalworking machines'),
-	('Tvárniace stroje', 'Forming machines'),
-	('Stroje na ozubenie', 'Gear machines'),
-	('Ostatné stroje', 'Other machines'),
-	('Príslušenstvo', 'Accessories');
